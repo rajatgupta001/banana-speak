d939d22875e35e62ece2f273aa356d00e7a162c2
Rajat Gupta <dguptarajat@gmail.com>
2021-09-19 21:33:43 +0530

    final commit

--- app.js
+++ app.js
@@ -2,18 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text="
 
-function convertedText(text)
+//var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var url = "	https://api.funtranslations.com/translate/minion.json";
+
+function translationalURL(text)
 {
-    return url + "text";
+    return url + "?text=" + text;
+}
+
+
+function handlingError(error)
+{
+    console.log("error occured: ", error);
+    alert("server is down. Please try again after some time");
 }
 
 function clickHandler()
 {
-    fetch(convertedText(txtInput.value))
+    fetch(translationalURL(txtInput.value))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => console.log(json))
+    .then(json => {outputDiv.innerText = json.contents.translated})
+    .catch(handlingError)
 }
 
 btnTranslate.addEventListener("click", clickHandler());
