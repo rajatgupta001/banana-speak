085b7d77ab7e58040e3e4a7cba2283a68892db8d
Rajat Gupta <dguptarajat@gmail.com>
2021-09-19 08:55:19 +0530

    ex4: textarea input

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var btnTranslate = document.querySelector("#btn-translate");
-//console.log(btnTranslate);
+var btnTranslate = document.querySelector("#btn-translate"); 
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
 
 function clickHandler()
 {
     console.log("clicked");
+    console.log("input", txtInput.value);
 }
 
 btnTranslate.addEventListener("click", clickHandler());
